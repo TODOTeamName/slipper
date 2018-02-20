2dfbfdc512b909d2d8ac0ed86815606db06268f2
Dean Bassett <dean@deanveloper.com>
2018-02-20 13:45:05 -0500

    Made a very simple web server

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Config struct {
+	Host string
+}
 
 func main() {
-	fmt.Println("Ayyyy we're using Go")
+	// Read Config
+	configBytes, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		log.Fatal("Error while reading config: ", err)
+	}
+
+	// Parse Config JSON into struct
+	var config Config
+	err = json.Unmarshal(configBytes, &config)
+	if err != nil {
+		log.Fatal("Error while parsing config: ", err)
+	}
+
+	// Start HTTP Server :)
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(config.Host, nil))
+}
+
+// Handler function to handle HTTP Requests
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<body>lol</body>")
 }
