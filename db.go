4edb5387167c7c5ffaf592667cab94473ab3a506
root <root@JonPC.localdomain>
2018-02-23 13:54:25 -0500

    Added more to the db.go file

--- db.go
+++ db.go
@@ -1,1 +1,12 @@
-This is a file
+//http://go-database-sql.org/
+package main
+
+import "github.com/mattn/go-sqlite3"
+
+func addPackage( ){
+	db, err := go-sqlite3.open("sqlite3", "SERVER_NAME");
+	if err !+ nil{
+		log.Fatal(err);
+	}
+	db.close();
+}
