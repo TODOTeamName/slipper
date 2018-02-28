555f943bd7d617cbf0d381202e2beafc8aaa160d
Dean Bassett <dean@deanveloper.com>
2018-02-28 13:19:04 -0500

    use pointers to allow for nils

--- types.go
+++ types.go
@@ -3,23 +3,23 @@ package main
 type Config struct {
 	// Host of the application.
 	// Recommended: ":8080"
-	Host string
+	Host *string
 
 	// The root of where the web files are stored.
 	// Recommended: "web/"
-	Root string
+	Root *string
 
 	// Authentication Key used for Cookie Session Storage. Should
 	// be either 32 or 64 bytes long (encoded in UTF8)
-	CookieKey string
+	CookieKey *string
 
 	// The https info for the website.
 	// To use http instead of https (not recommended) put null here.
-	Https struct {
+	Https *struct {
 		// The location of the server's HTTPS cert.
-		Cert string
+		Cert *string
 
 		// The location of the server's HTTPS key.
-		Key string
+		Key *string
 	}
 }
