7e6846b264e196f5e9b09644a2809512e70a69a7
Dean Bassett <dean@deanveloper.com>
2018-03-21 13:21:44 -0400

    Added getpackage endpoint and removed dependency

--- endpoints.go
+++ endpoints.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/sessions"
 	"github.com/todoteamname/slipper/db"
 	"net/http"
 	"path"
 )
-
-var store *sessions.CookieStore
-
-func initCookieStore() {
-	store = sessions.NewCookieStore([]byte(*Settings.CookieKey))
-}
 
 func handlePackageAdd(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
@@ -58,4 +51,28 @@ func handlePackageRemove(w http.ResponseWriter, r *http.Request) {
 		"/pages/form_remove.html",
 	)
 	http.ServeFile(w, r, path.Join(*Settings.Root, "pages/form_remove.html"))
+}
+
+func handlePackageGet(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	form := r.Form
+
+	pack, err := db.GetPackage(form.Get("number"))
+	if err != nil {
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "Error 400: Bad Request. Database call went wrong.")
+		fmt.Fprintln(w, "Precise error:", err)
+		fmt.Fprintln(w, "Click <a href=\"/\">here</a> to go to the home page")
+		return
+	}
+
+	fmt.Fprintf(w,
+		"<script>history.replaceState(%q, %q, %q);</script>",
+		"asdf",
+		"Slipper|Update Package",
+		"/pages/form_remove.html",
+	)
+
+	fmt.Fprintf(w, "Package info: %+v<br><br>", pack)
+	fmt.Fprintln(w, "This is a temporary page")
 }
