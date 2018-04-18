70df34128b225f71d0b9ba931e7e6da241da49fb
Dean Bassett <dean@deanveloper.com>
2018-04-18 13:24:09 -0400

    Documentation

--- util.go
+++ util.go
@@ -4,6 +4,10 @@ import (
 	"net/http"
 )
 
+// Gets the building information from the session token.
+//
+// Returns the building that a request is logged into,
+// and a boolean indicating if it was successful.
 func getBuilding(r *http.Request) (string, bool) {
 	cookie, err := r.Cookie("session")
 	if err != nil {
